4c54769ab2a39d1af8ccda709acd47e7f8ff89ab
jdaev <jdhn42@gmail.com>
2021-12-07 11:09:40 -0500

    refactor(bundles): add test implemntation for tags
    PE-523

--- lib/src/utils/implementations/bundle_tag_parser_ffi.dart
+++ lib/src/utils/implementations/bundle_tag_parser_ffi.dart
@@ -2,4 +2,14 @@ import 'dart:typed_data';
 
 import 'package:arweave/arweave.dart';
 
-Uint8List serializeTags({required List<Tag> tags}) => throw UnimplementedError();
+import '../../utils.dart';
+
+/// ONLY FOR TESTING. WILL NOT PASS BUNDLE VERIFICATION
+Uint8List serializeTags({required List<Tag> tags}) {
+  // TODO: Add avro implementation
+  // This serialzation is onlt meant for testing.
+  // Any bundles created with this will not verify as this is not in the avro schema
+  return Uint8List.fromList(tags
+      .expand((t) => decodeBase64ToBytes(t.name) + decodeBase64ToBytes(t.value))
+      .toList());
+}

0a8366aac5333e402fd4a244744e15b018e26a39
Javed Hussein <jdhn42@gmail.com>
2022-03-22 16:47:08 -0400

    fix: fix nested json serialization PE-1136

--- lib/src/models/tag.dart
+++ lib/src/models/tag.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'tag.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class Tag {
   /// The tag's name encoded as Base64.
   final String name;
